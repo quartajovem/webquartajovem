c2f029baa4950ba01de80fe04b2e6c504f33c945
Leandro Machado <leandrovictoram@gmail.com>
2024-01-26 13:35:52 -0300

    feat: bannerHome

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import { BannerHome } from '@/components/BannerHome';
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <BannerHome />
     </main>
   );
 }
---
+++ src/components/BannerHome/index.jsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+
+export function BannerHome() {
+  return (
+    <section className="max-md:h-100 md:h-lvh w-full relative">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-500 z-20">
+        <h2 className="text-white text-center font-sans font-semibold px-10 text-2xl md:text-3xl uppercase transition-all duration-500">
+          Bem-vindo à
+        </h2>
+        <h1 className="text-white text-center font-sans font-extrabold px-10 text-6xl md:text-8xl uppercase transition-all duration-500">
+          Quarta Jovem
+          <br />
+          __UMP
+        </h1>
+      </div>
+      <div className="bg-qjblack opacity-65 max-md:h-100 md:h-lvh w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-500 z-10" />
+      <Image
+        className="max-md:h-100 md:h-lvh w-full object-cover overflow-hidden transition-all duration-500 z-0"
+        width={10000}
+        height={10000}
+        src="/bannerHome.jpg"
+        alt="Jovens reunidos com a bandeira da UMP"
+      />
+      {/* <video
+          src="/BackVideo.mp4"
+          controls={false}
+          type="video/mp4"
+          playsInline
+          autoPlay
+          loop
+          muted
+          className="max-md:h-100 md:h-lvh w-full object-cover overflow-hidden"
+        ></video> */}
+    </section>
+  );
+}

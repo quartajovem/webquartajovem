73edb410888eea428d560218fcbd68055139d49f
Leandro Machado <leandrovictoram@gmail.com>
2024-02-05 18:48:35 -0300

    build gap

--- src/components/SobreNos/index.jsx
+++ src/components/SobreNos/index.jsx
@@ -1,10 +1,10 @@
 export function SobreNos() {
   return (
-    <section className="w-full h-96 flex max-md:flex-col justify-center items-center bg-gradient-to-r from-qjred to-qjblue transition-all duration-300">
-      <div className="w-3/4 md:w-1/4 text-left md:pr-6 max-md:pb-3 text-5xl 2xl:text-8xl lg:text-6xl uppercase font-bold transition-all duration-300">
+    <section className="w-full h-96 flex max-md:flex-col justify-center gap-5 md:gap-10 items-center bg-gradient-to-r from-qjred to-qjblue transition-all duration-300">
+      <div className="w-3/4 md:w-1/4 text-left text-5xl 2xl:text-8xl lg:text-6xl uppercase font-bold transition-all duration-300">
         <h1 className="font-['Montserrat'] text-white">Quem somos?</h1>
       </div>
-      <div className="w-3/4 md:w-1/2 md:pl-12 transition-all duration-300">
+      <div className="w-3/4 md:w-1/2 transition-all duration-300">
         <p className="font-['Montserrat'] text-white text-sm 2xl:text-2xl lg:text-lg transition-all duration-300">
           A <b>Quarta Jovem</b> é uma sociedade interna da <b>Quarta Igreja Presbiteriana de Garanhuns</b>. Ligada a UMP
           (União de Mocidade Presbiteriana), tem como integrantes jovens de 18 a 35 anos.

ae4a95b48105c497a043c763e32f749b46a17e3b
Isaac Willian <isaaacwillian@gmail.com>
2024-02-05 19:12:58 -0300

    fix: responsividade

--- src/components/SobreNos/index.jsx
+++ src/components/SobreNos/index.jsx
@@ -1,8 +1,10 @@
 export function SobreNos() {
   return (
     <section className="w-full h-96 flex max-md:flex-col justify-center gap-5 md:gap-10 items-center bg-gradient-to-r from-qjred to-qjblue transition-all duration-300">
-      <div className="w-3/4 md:w-1/4 text-left text-5xl 2xl:text-8xl lg:text-6xl uppercase font-bold transition-all duration-300">
-        <h1 className="font-['Montserrat'] text-white">Quem somos?</h1>
+      <div className="max-md:w-3/4 text-left text-5xl 2xl:text-8xl lg:text-6xl uppercase font-bold transition-all duration-300">
+        <h1 className="font-['Montserrat'] text-white">
+          Quem <br /> somos?
+        </h1>
       </div>
       <div className="w-3/4 md:w-1/2 transition-all duration-300">
         <p className="font-['Montserrat'] text-white text-sm 2xl:text-2xl lg:text-lg transition-all duration-300">
